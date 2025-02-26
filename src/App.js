d9040e41da88d3843bdabc844e767166679dd306
Tilviya Thomas <tilviya@git.com>
2025-02-27 00:03:24 +0530

    Updated JavaScript files with corrections

--- src/App.js
+++ src/App.js
@@ -4,47 +4,87 @@ import BMIList from "./Components/bmi list";
 import { useState } from "react";
 
 function App() {
+  const [goalBmi, setGoalbmi] = useState({ type: "", weigt: "" });
   const [bmi, setbmi] = useState(0);
   const [bmitype, setbmiType] = useState("overweight");
-  const onFormdata = (w, h,) => {
-    let b =bmiCal(w,h);
+  const [bmiRange, setBmiRang] = useState({
+    underWeight: { low: "" },
+    normalweight: { low: "", high: "" },
+    overWeight: { low: "", high: "" },
+    obesityclassI: { low: "", high: "" },
+    obesityclassII: { low: "", high: "" },
+    obesityclassIII: { high: "" },
+  });
+  const onFormdata = (w, h) => {
+    let b = bmiCal(w, h);
     setbmi(b);
     let t = typeCal(b);
     setbmiType(t);
 
-    console.log(t,"your bmi",b,"height and weight", w,h);
-    
+    const range = {
+      underWeight:  { low: calWeight(18.5, h) },
+      normalweight: { low: calWeight(18.5, h), high: calWeight(24.9, h) },
+      overWeight: { low: calWeight(25, h), high: calWeight(29.9, h) },
+      obesityclassI: { low: calWeight(30, h), high: calWeight(34.9, h) },
+      obesityclassII: { low: calWeight(35, h), high: calWeight(39.9, h) },
+      obesityclassIII: { high: calWeight(40, h) },
+    };
+    setBmiRang(range);
+
+    setGoalbmi(weightChange(b, w, range));
   };
-  
 
   const bmiCal = (w, h) => {
-    return (w/(h * h)).toFixed(2);
-   console.log("BMI IS",bmiCal); 
-};
+    return (w / (h * h)).toFixed(2);
+  };
+  const calWeight = (b, h) => (b * h * h).toFixed(2);
+
+  const weightChange = (b, w, range) => {
+    let changeobj;
+    if (b > 24.9) {
+      changeobj = {
+        weigt:  (w - range.normalweight.high).toFixed(2),
+        type: "positive",
+      };
+      return changeobj;
+    } else if (b < 18.5) {
+      changeobj = {
+        weigt: (range.normalweight.low - w).toFixed(2),
+        type: "negative",
+      };
+      return changeobj;
+    } else {
+      changeobj = { weigt: 0, type: "normal" };
+      return changeobj;
+      
+    }
+   
+  };
+  console.log( "ggg",weightChange)
+
   const typeCal = (bmi) => {
-    if (bmi<18.9){
-      return "Under Weight"
-  }  else if(bmi>18.9 && bmi<29.9){
-      return "Normal"
-  }  else if (24.9 < bmi && bmi < 29.9) {
-    return "Over Weight";
-  } else if (29.9 < bmi && bmi < 34.9) {
-    return "Obesity Class I";
-  } else if (34.9 < bmi && bmi < 39.9) {
-    return "Obesity Class II";
-  } else if (bmi > 39.9) {
-    return "Obesity Class III";
-  }
+    if (bmi < 18.9) {
+      return "Under Weight";
+    } else if (bmi > 18.9 && bmi < 24.9) {
+      return "Normal";
+    } else if (24.9 < bmi && bmi < 29.9) {
+      return "Over Weight";
+    } else if (29.9 < bmi && bmi < 34.9) {
+      return "Obesity Class I";
+    } else if (34.9 < bmi && bmi < 39.9) {
+      return "Obesity Class II";
+    }
+
+ 
+
+ 
 };
 
+  // const [bcal, setbcal] = useState(00);
 
-
-    // const [bcal, setbcal] = useState(00);
-    
   // const [bmiscore,setbmiscore]=useState("00");
   // const bmical =(w,h)=>w+h;
   // console.log(bmical);
-
 
   // let bmi =18;
   //const changebmi =() =>{
@@ -60,15 +100,15 @@ function App() {
           <Form getdata={onFormdata} />
         </div>
 
-      <div className="row justify-content-center mt-5">
-        <div className=" col-12 col-sm-6 mb-5">
-          <Score bmiNo={bmi} bmiName={bmitype} /> 
+        <div className="row justify-content-center mt-5">
+          <div className=" col-12 col-sm-6 mb-5">
+            <Score bmiNo={bmi} bmiName={bmitype} uu={goalBmi} />
           </div>
 
           <div className=" col-12 col-sm-6 ">
-              <BMIList/>
-            </div>
+            <BMIList range={bmiRange} bmi={bmi} />
           </div>
+        </div>
       </div>
     </>
   );
--- src/Components/score.js
+++ src/Components/score.js
@@ -1,6 +1,6 @@
 
 
-function Score({ bmiNo, bmiName}) {
+function Score({ bmiNo, bmiName, uu}) {
 
   return (
     <div className="text-center shadow rounded p-4">
@@ -9,7 +9,17 @@ function Score({ bmiNo, bmiName}) {
         <div className="p-3 my-2 alert fs-1 alert-primary col-sm-4">{bmiNo}</div>
       </div>
       <div className="fs-3 fw-bold text-primary">{bmiName}</div>
+      {uu.type === "positive" && (
+        <div className="fs-4">"You need lose <span className="fw-bold">{uu.weigt} kg"</span> </div>
+      )}
+      {uu.type === "negative" && (
+        <div className="fs-4">"You need gain <span className="fw-bold">{uu.weigt} kg"</span> </div>
+      )}
+       {uu.type === "normal" && (
+        <div className="fs-4">"Your weight is Normal,Good for you" </div>
+      )}
     </div>  
+    
   );
 }
 
